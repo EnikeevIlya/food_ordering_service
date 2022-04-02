53a67ee5f9ec086eecda31dec31c1910503066ca
Admin <enikeevdev@gmail.com>
2022-04-02 11:12:55 +0600

    Initial commit. Created project.

---
+++ src/main/java/com/example/java_group_11_controlwork_7_ilya_enikeev/JavaGroup11Controlwork7IlyaEnikeevApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_group_11_controlwork_7_ilya_enikeev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGroup11Controlwork7IlyaEnikeevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaGroup11Controlwork7IlyaEnikeevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/java_group_11_controlwork_7_ilya_enikeev/JavaGroup11Controlwork7IlyaEnikeevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_group_11_controlwork_7_ilya_enikeev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGroup11Controlwork7IlyaEnikeevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

d2a5d9ee6741b9097df552ded014bbbdbf0d7cc7
Admin <enikeevdev@gmail.com>
2022-04-02 11:37:23 +0600

    Added security and swagger configurations

---
+++ src/main/java/com/example/java_group_11_controlwork_7_ilya_enikeev/configuration/SecurityConfiguration.java
@@ -0,0 +1,38 @@
+package com.example.java_group_11_controlwork_7_ilya_enikeev.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Bean
+    public PasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers("/publications/**", "/likes/**", "comments/**")
+//                .fullyAuthenticated();
+//
+//        http.authorizeRequests()
+//                .anyRequest()
+//                .permitAll();
+//
+//        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//
+//        http.httpBasic();
+//
+//        http.formLogin().disable().logout().disable();
+//
+//        http.csrf().disable();
+    }
+}
